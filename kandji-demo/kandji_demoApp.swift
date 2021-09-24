1911db06ee0f7c2c519f9a80d9d2d73e03b64af0
twodayslate <zac@gorak.us>
2021-09-23 20:00:48 -0400

    Initial Commit

---
+++ kandji-demo/kandji_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  kandji_demoApp.swift
+//  kandji-demo
+//
+//  Created by Zachary Gorak on 9/23/21.
+//
+
+import SwiftUI
+
+@main
+struct kandji_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
